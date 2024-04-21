082d34ca16c701bd4c1a3e5f447aae0f9c807ab2
JorgenDIF <jorgenjonsson80@gmail.com>
2024-04-21 08:25:53 +0200

    Style all-auth pages using crispy forms
    Enhanced form aesthetics and usability with crispy forms integration.

--- main/settings.py
+++ main/settings.py
@@ -53,6 +53,10 @@ INSTALLED_APPS = [
 
     # Apps
     'home',
+
+    # Other
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
@@ -68,13 +72,17 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 ROOT_URLCONF = "main.urls"
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "templates")
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth")
             ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -83,6 +91,10 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+            ],
+            "builtins": [
+                "crispy_forms.templatetags.crispy_forms_tags",
+                "crispy_forms.templatetags.crispy_forms_field"
             ],
         },
     },
