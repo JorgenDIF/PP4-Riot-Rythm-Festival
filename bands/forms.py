2eba572d07e8c8e68960de173237275bf4d6b966
JorgenDIF <jorgenjonsson80@gmail.com>
2024-04-23 12:42:41 +0200

    Create forms and urls in bands app
    Added forms.py and urls.py for band creation and updated views.py to handle new form submission.

---
+++ bands/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from djrichtextfield.widgets import RichTextWidget
+from .models import Band
+
+
+class BandsForm(forms.ModelForm):
+    class Meta:
+        model = Band
+        fields = ['band_name',
+                  'description', 'genre_choices', 'image', 'image_alt', ]
+
+
+    description = forms.CharField(widget=RichTextWidget())
+
+    widget = {
+        'description': forms.Textarea(attrs={'rows': 5}),
+    }
+
+    labels = {
+        'band_name': 'Band Name',
+        'description': 'Description',
+        'genre_choices': 'Genre',
+        'image': 'Image',
+        'image_alt': 'Image Alt',
--- bands/views.py
+++ bands/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from .models import Band
+
+
+class AddBand(CreateView):
+    """ Add band view """
+    template_name = 'bands/add_band.html'
+    model = Band
+    success_url = '/bands/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddBand, self).form_valid(form)
+
+
