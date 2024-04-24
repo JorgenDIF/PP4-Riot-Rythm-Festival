dca2882de35108d0fc66f19ae85f984817d33915
JorgenDIF <jorgenjonsson80@gmail.com>
2024-04-24 13:19:55 +0200

    Add BandRequest model for handling user band requests
    This commit introduces the BandRequest model to manage user requests for bands. The model includes fields for the request ID, user, band, motivation, request date, and a unique slug. The slug is generated by combining the band's name with the request ID to ensure uniqueness. This model allows users to express their interest in specific bands, which can then be reviewed and managed by site administrators. The motivation behind this addition is to enhance user engagement and provide a structured way for users to communicate their preferences, thereby improving the overall user experience.

--- bands/models.py
+++ bands/models.py
@@ -21,6 +21,11 @@ GENRE_CHOICES = (
 
 
 class Band(models.Model):
+    """
+    A model representing a band with inspiration from the Macs YouTube video.
+    Link to video is in the README.md file.
+    """
+
     band_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     band_name = models.CharField(max_length=255)
@@ -53,3 +58,31 @@ class Band(models.Model):
 
     def __str__(self):
         return self.band_name
+
+
+"""
+A model representing a band_request with inspiration from the Macs YouTube
+video. Link to video is in the README.md file.
+
+"""
+
+
+class BandRequest(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+    motivation = models.CharField(max_length=255)
+    request_date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.band.band_name + "-" + str
+                                (self.request_id))
+        super(BandRequest, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['request_date']
+
+    def __str__(self):
+        return f"Request {self.request_id} for {self.band.band_name}"
