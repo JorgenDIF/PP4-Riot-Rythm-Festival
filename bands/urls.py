4dc24833bccf526ab71bd25e3c61a9a6372651af
JorgenDIF <jorgenjonsson80@gmail.com>
2024-05-04 08:23:55 +0200

    refactor: Update URL patterns for enhanced accessibility
    Updated the URL pattern to include both <int:pk> and <slug:slug> for a more descriptive and accessible URL structure. This change allows for the use of both unique identifiers and human-readable slugs, improving SEO and user experience.

--- bands/urls.py
+++ bands/urls.py
@@ -19,10 +19,10 @@ from .views import (
 urlpatterns = [
     path("", AddBand.as_view(), name="add_band"),
     path("band_bank/", Bands.as_view(), name="band_bank"),
-
-    path("bands/<int:pk>/", BandDetail.as_view(), name="band_detail"),
-    path("bands/<int:pk>/delete/", DeleteBand.as_view(),
+    path("bands/<int:pk>/<slug:slug>/", BandDetail.as_view(),
+         name="band_detail"),
+    path("bands/<int:pk>/<slug:slug>/delete/", DeleteBand.as_view(),
          name="delete_band"),
-    path("bands/<int:pk>/edit/", EditBand.as_view(), name="edit_band"),
-
+    path("bands/<int:pk>/<slug:slug>/edit/", EditBand.as_view(),
+         name="edit_band"),
 ]
